3af61d1a1848f3e81852ef0b7b017e816d7a1695
tassa <ftassani@correo.um.edu.uy>
2024-08-15 12:30:35 -0300

    agrego el posta

---
+++ tic1/src/main/java/org/example/tic1/Tic1Application.java
@@ -0,0 +1,13 @@
+package org.example.tic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tic1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tic1Application.class, args);
+	}
+
+}
